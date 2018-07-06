d9eff3a8acc2dfc8596f685704c29ffa00b3d84b
alirezaasghari <alireza.asghari87@gmail.com>
2018-07-06 14:25:23 +0200

    Finish #6, Toggling and debugging

--- cypress/integration/list-items.spec.js
+++ cypress/integration/list-items.spec.js
@@ -12,12 +12,12 @@ describe('List items', () => {
             .should('be.checked')
     })
 
-    it.only('Shows remaining todos in the footer', () => {
+    it('Shows remaining todos in the footer', () => {
         cy.get('.todo-count')
             .should('contain', 3)
     })
 
-    it.only('Removes a todo', () => {
+    it('Removes a todo', () => {
        cy.route({
            url: '/api/todos/1',
            method: "DELETE",
@@ -39,4 +39,34 @@ describe('List items', () => {
             .and('not.contain', 'Milk')
 
     })
+
+    it.only('Marks an incomplete item complete', () => {
+        cy.fixture('todos')
+            .then(todos => {
+                const target = Cypress._.head(todos)
+                cy.route(
+                    'PUT',
+                    `/api/todos/${target}`,
+                    Cypress._.merge(target, {isComplete: true})
+                )
+            })
+
+        cy.get('.todo-list li')
+            .first()
+            .as('first-todo')
+
+        cy.get('@first-todo')
+            .find('.toggle')
+            .click()
+            .should('be.checked')
+
+        cy.get('@first-todo')
+            .should('have.class', 'completed')
+
+        cy.get('.todo-count')
+            .should('contain', 2)
+
+    })
+
+
 })
